fcd709ebb210f9d7740276b9e1a870f223333aae
vivek <vivek254258@gmail.com>
2020-09-11 10:05:46 +0530

    hello Signed-off-by: vivek <vivek254258@gmail.com>

--- test 2.py
+++ test 2.py
@@ -2,10 +2,13 @@ from tkinter import *
 
 root = Tk()
 
+def vivek():
+    print("hi")
 
 root.title("window")
 root.geometry('200x200')
 Button(root,text ='click',bg = 'yellow',command = root.destroy).pack(padx = 10)
+Button(root,text = "click",command = vivek).pack()
 
 
 mainloop()
