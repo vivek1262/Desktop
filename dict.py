3990c25d3d80b7f99c494ce00e6596bf36f33b6b
vivek <vivek254258@gmail.com>
2020-10-19 11:55:20 +0530

    Signed-off-by: vivek <vivek254258@gmail.com>

---
+++ dict.py
@@ -0,0 +1,23 @@
+# fruits = {'apple':3,'mangos':3,'watermelon':1}
+#
+# for k,v in fruits.items():
+#     print( f"I have {v} {k}")
+
+
+# from string import ascii_lowercase as lower
+#
+# key = {}
+# for i in range(len(lower)):
+#     key[lower[i]] = 1+i
+
+# def cube(func):
+#     def wrapper(arg):
+#         return func(arg)**3
+#     return wrapper
+# @cube
+# def num(x):
+#     return x
+
+
+
+
